a5a91baa2aa70d3722a1801f2c5ddeac69eb207c
MaRisa Jackson <mhljackson@gmail.com>
2014-08-26 15:18:03 -0500

    Add authentication and authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def create
   	@user = User.new(user_params)
   	if @user.save
@@ -10,6 +13,9 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+  end
+
   def new
   	@user = User.new
   end
@@ -18,9 +24,33 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location  
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
